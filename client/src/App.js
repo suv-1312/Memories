bff545db296b7bb6568e8ae4fb6297b867ad3d92
Suvrat <saxenasuvrat@gmail.com>
2022-08-30 00:52:26 +0530

    Learning Development

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import Owl from './images/Owl.webp';
+import useStyles from './styles';
+
+
+const App = () => {
+
+    const classes = useStyles();
+
+    return(
+        <container maxWidth = "lg">
+            <AppBar className = {classes.appBar} position="static" color = "inherit">
+                <Typography variant = "h2" align = "center">Memories</Typography>
+                <img className = {classes.image} src = {Owl} alt ="memories"  width = "100" height = "100"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems='stretch' spacing = {3}>
+                        <Grid item xs={12} sm = {7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm = {4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+        </container>
+    )
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+//initialize the app
+const app = express();
+
+//middleware to connect to our application
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit:"30mb", extended: "true"}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: "true"}));
+app.use(cors());
+
+
+//connection with database
+
+const CONNECTION_URL = 'mongodb://suv1312:suv1312@ac-8vlni3u-shard-00-00.ll0ie35.mongodb.net:27017,ac-8vlni3u-shard-00-01.ll0ie35.mongodb.net:27017,ac-8vlni3u-shard-00-02.ll0ie35.mongodb.net:27017/?ssl=true&replicaSet=atlas-h4w8tj-shard-0&authSource=admin&retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+ .then( () => app.listen(PORT, () => console.log(`Server Running on port : ${PORT}`) ))
+ .catch((error) => console.log(error.message));
+
+
+ //mongoose.set('useFindAndModify',false);
